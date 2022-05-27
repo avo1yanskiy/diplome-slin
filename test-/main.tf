7bf7310de0310610abb5c54f34c7a3d655845787
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2022-05-27 18:23:08 +0300

    add test

--- test-/main.tf
+++ test-/main.tf
@@ -82,42 +82,6 @@ resource "yandex_compute_instance" "vm-3" {
   }
 }
 
-resource "yandex_compute_instance" "vm-5" {
-
-  name        = "linux-vm"
-
-  resources {
-    cores  = "2"
-    memory = "2"
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd87kbts7j40q5b9rpjr"
-    }
-  }
-
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
-    nat       = true
-  }
-
-  metadata = {
-    user-data = "${file("./meta.txt")}"
-  }
-}
-
-resource "yandex_vpc_network" "network1" {
-  name = "backnetwork"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name       = "subnet1"
-  zone       = "ru-central1-b"
-  network_id = "${yandex_vpc_network.network-1.id}"
-}
-
-
 resource "yandex_vpc_network" "network1" {
   name = "backnetwork"
 }
