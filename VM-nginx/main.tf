2d948abb2611f608e3c0b1229883f7fcf653dd3f
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2022-05-27 16:00:38 +0300

    rm & add

--- VM-nginx/main.tf
+++ VM-nginx/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "-L6EQ"
+  token     = "AQAAAABYXzwUAATuwScIJzAPv0G3nSn89s-L6EQ"
   cloud_id  = "b1gkdhnrl4d9t6mveu21"
   folder_id = "b1guvngenjnqosl63v2v"
   zone      = "ru-central1-a"
@@ -75,43 +75,4 @@ resource "yandex_vpc_subnet" "subnet-1" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.100.0/24"]
-}
-
-resource "yandex_lb_target_group" "foo" {
-  name      = "my-target-group"
-  region_id = "ru-central1"
-
-  target {
-    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
-    address   = "${yandex_compute_instance.vm-1.network_interface.0.ip_address}"
-  }
-
-  target {
-    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
-    address   = "${yandex_compute_instance.vm-2.network_interface.0.ip_address}"
-  }
-}
-
-resource "yandex_lb_network_load_balancer" "foo" {
-  name = "my-network-load-balancer"
-
-  listener {
-    name = "ngixn"
-    port = 80
-    external_address_spec {
-      ip_version = "ipv4"
-    }
-  }
-
-  attached_target_group {
-    target_group_id = "${yandex_lb_target_group.foo.id}"
-
-    healthcheck {
-      name = "http"
-      http_options {
-        port = 80
-        path = "/"
-      }
-    }
-  }
 }
