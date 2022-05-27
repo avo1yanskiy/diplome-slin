d2fc1eb31527c66eb24d9f586e07e8d0f55042b5
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2022-05-27 16:43:33 +0300

    Update main.tf

--- VM-nginx/main.tf
+++ VM-nginx/main.tf
@@ -64,60 +64,6 @@ resource "yandex_compute_instance" "vm-2" {
   }
 }
 
-resource "yandex_iam_service_account" "main" {
-  name        = "main"
-  description = "service account"
-}
-
-resource "yandex_resourcemanager_folder_iam_binding" "editor" {
-  folder_id = "b1guvngenjnqosl63v2v"
-  role      = "editor"
-  members   = [
-    "serviceAccount:${yandex_iam_service_account.main.id}",
-  ]
-}
-
-resource "yandex_compute_instance_group" "ig-1" {
-  name               = "fixed-ig-with-balancer"
-  folder_id          = "<идентификатор каталога>"
-  service_account_id = "${yandex_iam_service_account.main.id}"
-    resources {
-      memory = "2"
-      cores  = "2"
-    }
-  }
-    boot_disk {
-      mode = "READ_WRITE"
-      initialize_params {
-        image_id = "fd87kbts7j40q5b9rpjr"
-      }
-    }
-
-metadata = {
-    user-data = "${file("./meta.txt")}"
-  }
-
-scale_policy {
-    fixed_scale {
-      size = "2"
-    }
-  }
-
-  allocation_policy {
-    zones = ["ru-central1-a"]
-  }
-
-  deploy_policy {
-    max_unavailable = 1
-    max_expansion   = 0
-  }
-
-  load_balancer {
-    target_group_name        = "target-group"
-    target_group_description = "load balancer target group"
-  }
-}
-
 resource "yandex_vpc_network" "network1" {
   name = "backnetwork"
 }
