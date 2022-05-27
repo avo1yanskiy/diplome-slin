7ec7cb6725513534a3f4e450f45b57c835140b58
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2022-05-27 16:07:46 +0300

    Update main.tf

--- VM-nginx/main.tf
+++ VM-nginx/main.tf
@@ -66,13 +66,13 @@ resource "yandex_compute_instance" "vm-2" {
   }
 }
 
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
+resource "yandex_vpc_network" "network1" {
+  name = "backnetwork"
 }
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
+  network_id     = yandex_vpc_network.network1.id
   v4_cidr_blocks = ["192.168.100.0/24"]
 }
