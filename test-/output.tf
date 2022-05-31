7e6ee04516378df1371a1b6b2236a578c19df7b8
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2022-05-31 15:47:34 +0300

    update

--- test-/output.tf
+++ test-/output.tf
@@ -1,5 +1,5 @@
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+output "internal_ip_address_virtual_machine_1" {
+  value = yandex_compute_instance.virtual-machine-1.network_interface.0.ip_address
 }
 
 output "internal_ip_address_vm_2" {
@@ -23,8 +23,8 @@ output "internal_ip_address_vm_8" {
   value = yandex_compute_instance.vm-8.network_interface.0.ip_address
 }
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "external_ip_address_virtual_machine_1" {
+  value = yandex_compute_instance.virtual-machine-1.network_interface.0.nat_ip_address
 }
 
 output "external_ip_address_vm_2" {
