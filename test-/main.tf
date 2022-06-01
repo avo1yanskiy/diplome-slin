bdc5e6f63ce19c1ed1ee589d9b71452fe790ce9e
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2022-06-01 21:03:19 +0300

    Update main.tf

--- test-/main.tf
+++ test-/main.tf
@@ -272,7 +272,21 @@ resource "yandex_alb_http_router" "tf-router" {
   name      = "my-http-router"
   labels = {
     tf-label    = "tf-label-value"
-    empty-label = ""s
+    empty-label = ""
+  }
+}
+
+resource "yandex_alb_virtual_host" "my-virtual-host" {
+  name      = "my-virtual-host"
+  http_router_id = yandex_alb_http_router.tf-router.id
+  route {
+    name = "my-route"
+    http_route {
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.test-backend-group.id
+        timeout = "3s"
+      }
+    }
   }
 }
 
