371d915825676f4c7a3d7c3a103b49db12b9fee3
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2022-06-01 11:46:24 +0300

    rm code port

--- virtual-machine/main.tf
+++ virtual-machine/main.tf
@@ -14,12 +14,13 @@ provider "yandex" {
   zone      = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "vm-1" {
+resource "yandex_compute_instance" "virtual-machine-1" {
   name = "elasticsearch"
+  zone = "ru-central1-a"
 
   resources {
     cores  = 2
-    memory = 4
+    memory = 2
   }
 
   boot_disk {
@@ -30,7 +31,7 @@ resource "yandex_compute_instance" "vm-1" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
+    ip_address = "192.168.101.5"
   }
 
   metadata = {
@@ -38,12 +39,13 @@ resource "yandex_compute_instance" "vm-1" {
   }
 }
 
-resource "yandex_compute_instance" "vm-2" {
+resource "yandex_compute_instance" "virtual-machine-2" {
   name = "grafana"
+  zone = "ru-central1-a"
 
   resources {
     cores  = 2
-    memory = 4
+    memory = 2
   }
 
   boot_disk {
@@ -54,16 +56,21 @@ resource "yandex_compute_instance" "vm-2" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
+    ip_address = "192.168.101.6"
+  }
+
+metadata = {
+    user-data = "${file("./meta.txt")}"
   }
 }
 
-resource "yandex_compute_instance" "vm-3" {
+resource "yandex_compute_instance" "virtual-machine-3" {
   name = "prometheus"
+  zone = "ru-central1-a"
 
   resources {
     cores  = 2
-    memory = 4
+    memory = 2
   }
 
   boot_disk {
@@ -74,11 +81,134 @@ resource "yandex_compute_instance" "vm-3" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
+    ip_address = "192.168.101.7"
   }
 
   metadata = {
     user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "virtual-machine-4" {
+  name = "http-1"
+  zone = "ru-central1-a"
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87kbts7j40q5b9rpjr"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+    ip_address = "192.168.101.10"
+  
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "virtual-machine-5" {
+  name = "http-2"
+  zone = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87kbts7j40q5b9rpjr"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-2.id}"
+    nat       = true
+    ip_address = "192.168.100.8"
+    
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "virtual-machine-6" {
+  name = "kibana"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87kbts7j40q5b9rpjr"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+    ip_address = "192.168.101.9"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "virtual-machine-7" {
+  name = "bastion"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87kbts7j40q5b9rpjr"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+    nat       = true
+    ip_address = "192.168.101.100"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_vpc_security_group" "test-sg" {
+  name        = "Test security group"
+  description = "Description for security group"
+  network_id  = yandex_vpc_network.network1.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Rule description 1"
+    v4_cidr_blocks = ["192.168.101.0/24", "192.168.100.0/24"]
+    port           = 22
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "Rule description 2"
+    v4_cidr_blocks = ["192.168.101.0/24", "192.168.100.0/24"]
+    from_port      = 22
+    to_port        = 22
   }
 }
 
@@ -86,9 +216,56 @@ resource "yandex_vpc_network" "network1" {
   name = "backnetwork"
 }
 
+resource "yandex_vpc_subnet" "subnet-2" {
+  name       = "subnet2"
+  zone       = "ru-central1-b"
+  network_id = yandex_vpc_network.network1.id
+  v4_cidr_blocks = ["192.168.100.0/24"]
+
+}
+
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network1.id
-  v4_cidr_blocks = ["192.168.100.0/24"]
+  v4_cidr_blocks = ["192.168.101.0/24"]
+}
+
+resource "yandex_lb_target_group" "foo" {
+  name      = "my-target-group"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet-2.id}"
+    address   = "${yandex_compute_instance.virtual-machine-5.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+    address   = "${yandex_compute_instance.virtual-machine-4.network_interface.0.ip_address}"
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "foo" {
+  name = "my-network-load-balancer"
+
+  listener {
+    name = "nginx"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.foo.id}"
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
 }
